29cc477d62871ef77eac60d9017b935d94454f61
iadokonat <iadokonat0106@gmail.com>
2023-01-11 11:09:48 +0900

    Use the get_object_or_404 function

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -11,10 +11,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except:
-        raise Http404("質問が存在しません")
+    question = get_object_or_404(Question, pk=question_id)
     context = {"question": question}
     return render(request, "polls/detail.html", context)
 
