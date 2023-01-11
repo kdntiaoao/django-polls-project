82a3796eebd74993fe63f94c4fd07de7eff9a7b9
iadokonat <iadokonat0106@gmail.com>
2023-01-11 12:18:16 +0900

    Fix urls

--- polls/urls.py
+++ polls/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<int:pk>/", views.detail, name="detail"),
     path("<int:pk>/results/", views.results, name="results"),
-    path("<int:pk>/vote/", views.vote, name="vote"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
